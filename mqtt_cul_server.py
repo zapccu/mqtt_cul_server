027e9e395f920e730bfc71e5f8a4daa9a8e05e07
zapccu <d.braner@gmx.net>
2024-11-12 19:36:20 +0100

    Minor changes

--- mqtt_cul_server.py
+++ mqtt_cul_server.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     else:
         logger.setLevel(level)
     
-	# Signal handler
+    # Signal handler
     signal.signal(signal.SIGTERM, signal_handler)
 
     mcs = MQTT_CUL_Server(config=config)
